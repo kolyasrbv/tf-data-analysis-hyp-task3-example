39aacd6c5b8049faad938710bfab3951fa0de66c
Nikolay Serebryakov <116488084+kolyasrbv@users.noreply.github.com>
2023-04-13 14:57:28 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import numpy as np
-
+import scipy.stats as st
 
 chat_id = 433193277 # Ваш chat ID, не меняйте название переменной
 
-def solution(x1: np.array) -> bool: 
-    result = ztest(x1, value = 500, alternative = 'larger')[1] 
+def solution(x: np.array) -> bool:
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    alpha = 0.04
     
-    if result < 0.04:
-        return True
-    return False
-solution(x1)
+    # порог доходности нового продукта
+    threshold = 500
+    
+    # вычисляем t-статистику и p-value
+    t_statistic, p_value = st.ttest_1samp(x, threshold, alternative="two-sided")
+    return p_value < 2*alpha and x.mean() > threshold
+
+
+433193277
